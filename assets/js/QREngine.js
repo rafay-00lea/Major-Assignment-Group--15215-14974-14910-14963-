ab6689d1c4ce4ca2915613700f566fe4b4b3d126
muhammad-atif-khan-1 <matifkhan1068@gmail.com>
2025-12-11 19:56:44 +0500

    fix: correct QREngine class structure and methods

--- assets/js/QREngine.js
+++ assets/js/QREngine.js
@@ -1,47 +1,46 @@
 /*
  * QREngine.js
+ * Implements CLO 4: Object-Oriented Programming Principles
  */
 
 class QREngine {
   constructor(elementId) {
     this.container = document.getElementById(elementId);
     this.qrCodeObj = null;
-}
+  }
 
-}
-generate(text, options) {
-    // TODO: implement QR generation
-    this.container.innerHTML = "";
+  // Generates a new QR code
+  generate(text, options) {
+    this.container.innerHTML = ""; // Clear previous QR
 
     if (!text) {
-    alert("Please enter text or URL");
-    return;
-}
+      alert("Please enter text or URL");
+      return;
+    }
 
-}
+    // Create new QRCode instance
+    this.qrCodeObj = new QRCode(this.container, {
+      text: text,
+      width: parseInt(options.size),
+      height: parseInt(options.size),
+      colorDark: options.color,
+      colorLight: options.bgColor,
+      correctLevel: QRCode.CorrectLevel.H
+    });
+  }
 
-this.qrCodeObj = new QRCode(this.container, {
-    text: text,
-    width: parseInt(options.size),
-    height: parseInt(options.size),
-    colorDark: options.color,
-    colorLight: options.bgColor,
-    correctLevel: QRCode.CorrectLevel.H
-});
-
-downloadQR(fileName = "qrcode.png") {
-    // TODO
+  // Downloads the generated QR code
+  downloadQR(fileName = "qrcode.png") {
     const img = this.container.querySelector("img");
-if (img && img.src) {
-    const link = document.createElement("a");
-    link.href = img.src;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-} else {
-    alert("No QR code generated yet!");
+    if (img && img.src) {
+      const link = document.createElement("a");
+      link.href = img.src;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      alert("No QR code generated yet!");
+    }
+  }
 }
-
-}
-

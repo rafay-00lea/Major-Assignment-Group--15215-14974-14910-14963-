ead0df3460c2a084e7bacdab5bf038e0f24acc34
muhammad-atif-khan-1 <matifkhan1068@gmail.com>
2025-12-11 21:47:47 +0500

    feat: select DOM elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,14 @@
  */
 // 1. Initialize the QREngine Class
 const qrEngine = new QREngine("qr-output");
+
+
+// 2. Select DOM Elements
+const textInput = document.getElementById("qr-text");
+const colorInput = document.getElementById("qr-color");
+const bgInput = document.getElementById("qr-bgcolor");
+const sizeInput = document.getElementById("qr-size");
+const generateBtn = document.getElementById("generate-btn");
+const downloadBtn = document.getElementById("download-btn");
+const historyList = document.getElementById("history-list");
+const clearHistoryBtn = document.getElementById("clear-history");
